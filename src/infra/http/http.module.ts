f1b56d6089d8381fa64c43a1a3dc7910a3beff70
MatthewAraujo <matthewaraujo20@gmail.com>
2024-08-29 14:04:17 -0300

    feat: routersn

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,7 +3,8 @@ import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionController } from './controllers/fetch-recent-questions.controller'
-import { DatabaseModule } from '../database/datavase.module'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
   imports: [DatabaseModule],
@@ -13,5 +14,6 @@ import { DatabaseModule } from '../database/datavase.module'
     CreateQuestionController,
     FetchRecentQuestionController,
   ],
+  providers: [CreateQuestionUseCase],
 })
-export class HttpModule {}
+export class HttpModule { }
